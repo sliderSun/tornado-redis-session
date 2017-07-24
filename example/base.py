561e888c0c30c25b95ebf42709910f893d955e99
孙树兵 <291952004@qq.com>
2017-07-24 13:37:15 +0800

    Update base.py

--- example/base.py
+++ example/base.py
@@ -4,10 +4,12 @@ sys.path.append("..")
 import session
 
 
-class BaseHandler(tornado.web.RequestHandler):
-	def __init__(self, *argc, **argkw):
-		super(BaseHandler, self).__init__(*argc, **argkw)
-		self.session = session.Session(self.application.session_manager, self)
-
-	def get_current_user(self):
-		return self.session.get("user_name")
+class BaseHandler(tornado.web.RequestHandler):   
+	def __init__(self, *argc, **argkw):   
+    		super(BaseHandler, self).__init__(*argc, **argkw)   
+   		 # 定义 handler 的 session, 注意，根据 HTTP 特点，每次访问都会初始化一个 Session 实例哦，这对于你后面的理解很重要   
+    		self.session = session.Session(self.application.session_manager, self)   
+   
+  	# 这是干嘛的？用于验证登录...请 google 关于 tornado.web.authenticated, 其实就是 tornado 提供的用户验证   
+	def get_current_user(self):   
+    		return self.session.get("user_name")  
